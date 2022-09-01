ab8a4bdd049623fb3d36bee1b683dfc6081c6044
Pablo Cosin <pcosin@gmail.com>
2022-09-01 10:56:01 -0300

    clase 8 sin ruta detail

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -3,12 +3,13 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import CartWidget from './CartWidget';
 import { NavLink } from 'react-router-dom';
+import logo from "./../../logo.jpg"
 
 export const NavBar = () => {
   return (
     <Navbar bg="light" expand="lg">
       <Container className=''>
-      <NavLink to="/"><img className='logo' src="img/logo.jpg" alt="" /></NavLink>
+      <NavLink to="/"><img className='logo' src={logo} alt="" /></NavLink>
         
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav" className='nav-links'>

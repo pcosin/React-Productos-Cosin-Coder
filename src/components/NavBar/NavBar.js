146ece98b931bff46eca4dcb82a25622a0c6d0a1
Pablo Cosin <pcosin@gmail.com>
2022-09-04 09:22:46 -0300

    navba categorias fixed

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,6 +1,7 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 import CartWidget from './CartWidget';
 import { NavLink } from 'react-router-dom';
 import logo from "./../../logo.jpg"
@@ -14,9 +15,12 @@ export const NavBar = () => {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav" className='nav-links'>
           <Nav className="mx-auto">
-          <NavLink className="navlink" to="categoria/autos">Autos</NavLink>
-          <NavLink className="navlink" to="categoria/otros">Otros</NavLink>
-
+          <NavDropdown title="Categorias" id="basic-nav-dropdown">
+              <NavDropdown.Item><NavLink className="navlink" to="categoria/autos">Autos</NavLink></NavDropdown.Item>
+              <NavDropdown.Item>          <NavLink className="navlink" to="categoria/otros">Otros</NavLink>
+</NavDropdown.Item>
+            </NavDropdown>
+          
           </Nav>
           <NavLink className="navlink carritoNavBar" to="/cart"><CartWidget /></NavLink>
         </Navbar.Collapse>

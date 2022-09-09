a7adf84256a45f1e1852aece4d2664dd09f7290a
Pablo Cosin <pcosin@gmail.com>
2022-09-09 11:01:37 -0300

    clase 10 funcionando

--- src/components/Main/cart/Cart.js
+++ src/components/Main/cart/Cart.js
@@ -1,18 +1,55 @@
 import React from 'react'
 import { useCartContex } from "../../../context/CartContext";
+import { ListGroup, ListGroupItem, Container, Card } from 'react-bootstrap';
+import { Link, NavLink } from 'react-router-dom';
+
 
 function Cart() {
 
-  const { cartList, clearCart, isIncart } = useCartContex()
+  const { cartList, clearCart, isIncart, removeItem } = useCartContex()
 
   console.log(cartList)
+  
   return (
-    <>
-    <div style={{ display: 'flex', justifyContent: 'center' }}>
-    <button onClick={clearCart}>Clear Cart</button>
+    <div>
+    {cartList.length === 0 ? <div> <h2>"No hay nada en el carrito"</h2>
+    <Link to="/">
+    <button>Volver a comprar</button>
+    </Link></div>
+        :
+
+        
+cartList.map(item =>
+
+<Container className="card-boostrap">
+
+<Card style={{ width: '18rem' }}>
+<Card.Img variant="top" src={item.imagen} />
+<Card.Body key={item.id} >
+<Card.Title>{item.titulo}</Card.Title>
+<Card.Text>
+    Cantida de items del producto: {item.cantidad}
+
+</Card.Text>
+<Card.Text>
+
+</Card.Text>
+<button onClick={() => removeItem(item.id)}>Borrar Item</button> 
+</Card.Body>
+
+</Card>
+
+<div> <button onClick={clearCart}>Limpiar Carrito</button> </div>
+
+<div>
 </div>
 
-</>
+
+</Container>)}
+      
+      
+
+    </div>
   )
 }
 
--- src/context/CartContext.js
+++ src/context/CartContext.js
@@ -8,34 +8,47 @@ const CartContextProvider = ({children}) => {
     const [cartList, setCarList] = useState([])
 
     const addToCart = (item, cantidad) => {
-        if(isInCart(item)) {
+        if(isInCart(item.id)) {
+
+            sumarCantidad(item, cantidad)
+
+            
             console.log("true")
 
         }
         else{
-            console.log("false")
+            setCarList( [ ...cartList, {...item, cantidad} ] )
         }
 
          
-      setCarList( [ ...cartList, {...item, cantidad} ] )
   }
 
+  const sumarCantidad = (item, cantidad) => {
+    const carritoActulizado = cartList.map((prod) => {
+        if(prod.id === item.id){
+            const productoActualizado = {...prod, cantidad: prod.cantidad + cantidad}
+            return productoActualizado
+        }else {
+                return prod
+        }
+    })
+    setCarList(carritoActulizado)
+  }
 
-    const isInCart = (item) => {
-        return cartList.find((element) => element.id === item.id) 
-     }
+    const isInCart = (id) => {
+    return cartList.some(prod => prod.id === id)     }
 
     const clearCart =() => {
         setCarList([])
     }
 
 
-    // const removeItem = (id) => {
-    //   setCarList (cartList.filter(item => item.id !== id))
-    // }
+    const removeItem = (id) => {
+      setCarList (cartList.filter(item => item.id !== id))
+    }
 
     return (
-        <CartContext.Provider value={{cartList, addToCart, clearCart, isInCart}} >
+        <CartContext.Provider value={{cartList, addToCart, clearCart, isInCart, removeItem}} >
             {children}
         </CartContext.Provider>
     );
